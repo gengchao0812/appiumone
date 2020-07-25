9b8e25175e817a7c677bf22800f69d79c5b9d09a
gengchao0812 <gengchao0812@126.com>
2020-07-26 02:23:13 +0800

    增加滑动

--- venv/test_dwpytest.py
+++ venv/test_dwpytest.py
@@ -2,6 +2,8 @@ import  pytest
 from appium import  webdriver
 import time
 import sys
+from  appium.webdriver.common.touch_action import TouchAction
+
 
 sys.path.append("..")
 
@@ -27,10 +29,57 @@ class TestDw():
     def teardown(self):
         self.driver.quit()
 
+    def test_touchaction(self):
+        print("开始等待5秒")
+        time.sleep(5)
+
+        action = TouchAction(self.driver)
+        #release 释放掉  perform 执行它 wait单位是毫秒
+        # action.press(x=731,y=2083).wait(200).move_to(x=731,y=484).release().perform()
+        #获取屏幕尺寸
+        windows_rect = self.driver.get_window_rect()['width']
+        width = self.driver.get_window_rect()['width']
+        height = self.driver.get_window_rect()['height']
+        x1 = int (width/2)
+        y_start = int(height * 4/5)
+        y_end = int (height * 1/5)
+        action.press(x=x1, y=y_start).wait(200).move_to(x=x1, y=y_end).release().perform()
+    
+    @pytest.mark.skip
+    def test_two(self):
+        """
+        1. 打开 雪球app
+        2. 定位首頁的搜索框
+        3. 判断搜索框的是否可用，并查看搜索框的name属性值
+        4. 打印搜索框这个元素的左上角坐标和他的宽高
+        5. 向搜索框输入alibaba
+        6. 判断【阿里巴巴】是否可见
+        7. 如果可见，打印“搜索成功”， 如果不可见，打印“搜索失败”
+        :return:
+        """
+        time.sleep(2)
+        el2 = self.driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+        print(el2.text)
+        print(f"宽和高是 {el2.size}")
+        print(f"元素坐标是 {el2.location}")
+        if el2.is_enabled() == True:
+            el2.click()
+        el3 = self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("alibaba")
+        el4 = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]")
+        #判断可见
+        elemengt_display = el4.get_attribute("displayed")
+        if elemengt_display == 'true':
+            print("搜索成功")
+        else:
+            print("搜索失败")
+
+
+    @pytest.mark.skip
     def test_search(self):
         print("搜索测试用例")
         """
-        1.打开 雪球app
+        1. 打开 雪球app
         2. 点击搜索输入框
         3. 向搜索输入框里输入“阿里巴巴”
         4. 在搜索结果里面选择“阿里巴巴”，然后进行点击
